43a211f4dc82c739b1da07b811dc06e4e9c531a2
Exptrns <Exptrns@gmail.com>
2023-07-08 15:13:51 +0300

    Добавлены файлы и папки

---
+++ C#Seminars/Seminar1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int a = 25; //целые числа
+//double b = 36.6; // вещественные числа
+//string s = "Hello World"; // строка
+//bool d = false;
+//int numb = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine(s);
