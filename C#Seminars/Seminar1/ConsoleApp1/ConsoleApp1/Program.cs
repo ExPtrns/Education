cf6c1dc4444b0049a70ee887a7116e2d00fbcb78
ExPtrns <138036225+ExPtrns@users.noreply.github.com>
2023-09-18 11:50:40 +0300

    Delete C#Seminars directory

--- C#Seminars/Seminar1/ConsoleApp1/ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-//int a = 25; //целые числа
-//double b = 36.6; // вещественные числа
-//string s = "Hello World"; // строка
-//bool d = false;
-//int numb = Convert.ToInt32(Console.ReadLine());
-
-//Console.WriteLine(s);
